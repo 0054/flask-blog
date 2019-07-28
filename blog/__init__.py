9a3d3cc9059ead7e4daaccfa53f783bcf4bc4243
0054 <kofe54@gmail.com>
2019-07-28 13:28:27 +0300

    add auth

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,7 +9,8 @@ app = Flask(__name__)
 # >>> secrets.token_hex(16)
 app.config['SECRET_KEY'] = 'fd807ecee9774c7027daed87270b24ed'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blog import routes
